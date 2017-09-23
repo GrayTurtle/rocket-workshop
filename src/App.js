e7b0b58e5718bb26df6245c71cd668f286a5fb5f
Josh Birdwell <jbirdwell34@gmail.com>
2017-09-23 09:44:34 -0400

    Add skelton for attendee box

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AttendeeBox from './AttendeeBox';
 
 class App extends Component {
   render() {
+    // FILL THIS OUT WITH STATIC DATE
+    const attendees = [];
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {attendees.map(attendee => <AttendeeBox status={attendee.status} step={attendee.step} username={attendee.username} />)}
       </div>
     );
   }
---
+++ src/AttendeeBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const AttendeeBox = ({ status, step, username }) => (
+  <div>
+    BOX
+  </div>
+);
+
+AttendeeBox.propTypes = {
+  
+};
+
+export default AttendeeBox;

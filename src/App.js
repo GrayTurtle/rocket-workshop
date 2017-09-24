44a30891d852a1d08c4c095425856f58d8abff2b
Josh Birdwell <jbirdwell34@gmail.com>
2017-09-24 02:23:01 -0400

    NIGHT

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import './App.css';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import {
-  BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
 import Authoring from './workshops/authoring';
 import Organizer from './organizer';
 import Presentation from './workshops/presentation';
+import Login from './authentication/login';
+import Signup from './authentication/signup';
+import Navbar from './navbar';
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <div>
+        <Navbar />
         <Switch>
           <Route exact path="/organizer/:organizerId" component={Organizer} />
           <Route path="/organizer/:organizerId/workshops/:workshopId/present" component={Presentation} />
           <Route path="/organizer/:organizerId/workshops/:workshopId/edit" component={Authoring} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
         </Switch>
-      </Router>
+      </div>
     );
   }
 }
--- src/organizer/workshop.js
+++ src/organizer/workshop.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 import Icon from 'react-icons-kit';
 import { ic_mode_edit } from 'react-icons-kit/md/ic_mode_edit'; 
 import { Link } from 'react-router-dom';
-
+import { withRouter } from 'react-router';
+import { ic_present_to_all } from 'react-icons-kit/md/ic_present_to_all';
+   
 import './workshop.css';
 
-const Workshop = ({ title, date, location, path }) => (
+const Workshop = ({ title, date, location, path, history }) => (
   <div className="workshop-single">
     <div className="workshop-title">{title}</div>
     <div className="workshop-date">{ date.toDateString() }</div>
+    <Link to={'/organizer/acm/workshops/23423d/present'} className="present-workshop">
+      <Icon icon={ic_present_to_all} />
+    </Link>
     <Link to={path} className="edit-workshop">
       <Icon icon={ic_mode_edit} />
     </Link>
@@ -20,4 +25,4 @@ Workshop.propTypes = {
   
 };
 
-export default Workshop;
+export default withRouter(Workshop);
